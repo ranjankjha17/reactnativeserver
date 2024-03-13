afde4018832d35e8df4eb8efc827c69fc90935c7
Ranjan Kumar Jha <talk2ranjanktr@gmail.com>
2024-03-13 22:09:09 +0530

    create login apis

--- masterRoutes.js
+++ masterRoutes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const multer = require('multer');
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
+const jwt = require('jsonwebtoken');
+const secretKey = "8794587125463258921456375"
 
 const dbService = require('./dbService');
 
@@ -76,6 +78,47 @@ const dbService = require('./dbService');
 //     return res.status(500).json({ error: "Failed to save data" });
 //   }
 // });
+
+router.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ message: 'Username and password are required' });
+  }
+
+  try {
+    const connection = await dbService.getConnection();
+    
+    const query = 'SELECT * FROM users WHERE username = ? AND password = ?';
+    const results = await executeQuery(connection, query, [username, password]);
+    connection.release()
+
+    if (results.length > 0) {
+      const token = jwt.sign({ username: username }, secretKey, { expiresIn: '6h' });
+      return res.status(200).json({ message: "User logged in successfully", username: username, success: true, token });
+    } else {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+const executeQuery = (connection, query, values) => {
+  return new Promise((resolve, reject) => {
+    connection.query(query, values, (err, results) => {
+      if (err) {
+        console.error('MySQL query error: ' + err.stack);
+        reject(err);
+      } else {
+        resolve(results);
+      }
+    });
+  });
+};
+
+
 
 router.post("/upload", upload.single('photo'), async (req, res) => {
   const { code, groupbc, rrnumber, name, cast, mobileno, id, photo } = req.body;
